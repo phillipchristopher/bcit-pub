9d572be41dc338c196ffcdb32aeaf20923b74005
Chris Helmhold <89236634+phillipchristopher@users.noreply.github.com>
2024-03-07 14:21:22 -0800

    Update Furniture.java

--- comp2501/lab9/Furniture.java
+++ comp2501/lab9/Furniture.java
@@ -1,19 +1,32 @@
-public class Furniture extends HouseholdObject
-{
+public class Furniture extends HouseholdObject {
   private final boolean isAssemblyRequired;
   private final String frameMaterial;
-  
+
   private static final String FRAME_WOOD = "wood";
   private static final String FRAME_METAL = "metal";
   private static final String FRAME_OTHER = "other";
-  
-  public Furniture (final String homeObjectName, 
-                    final String homeObjectBrand, 
-                    final double priceCad, 
-                    final String mainColour, 
-                    final boolean isAssemblyRequired, 
-                    final String frameMaterial)
-  {
+
+  public Furniture(final String homeObjectName,
+    final String homeObjectBrand,
+    final double priceCad,
+    final String mainColour,
+    final boolean isAssemblyRequired,
+    final String frameMaterial) {
     super(homeObjectName, homeObjectBrand, priceCad, mainColour)
   }
+
+  if (frameMaterial != null &&
+    (!frameMaterial.equalsIgnoreCase(FRAME_WOOD) &&
+      !frameMaterial.equalsIgnoreCase(FRAME_METAL) &&
+      !frameMaterial.equalsIgnoreCase(FRAME_OTHER))
+  ) {
+    throw new IllegalArgumentException("Invalid frame material" + frameMaterial);
+  }
+  this.isAssemblyRequired = isAssemblyRequired;
+  this.frameMaterial = frameMaterial;
+}
+
+@Override
+public String getType() {
+  return "Furniture";
 }

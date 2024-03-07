e0db019e15017315da50250c45b9620e6b28859b
Chris Helmhold <89236634+phillipchristopher@users.noreply.github.com>
2024-03-07 15:10:26 -0800

    Update Dishwasher.java

--- comp2501/lab9/Dishwasher.java
+++ comp2501/lab9/Dishwasher.java
@@ -3,19 +3,34 @@ public class Dishwasher extends Electronic
     private final String cycleTypes;
     private final int    decibalRating;
 
+    private static final String NORMAL_CYCLE = "normal";
+    private static final String HEAVY_DUTY_CYCLE = "heavy duty";
+    private static final String ECO_CYCLE = "eco";
+
+    private static final int MIN_DECIBAL_RATING = 0;
+    private static final int MAX_DECIBAL_RATING = 120;
+
     public Dishwasher(final int yearPurchased,
-                      final String createdBy,
-                      final String mainColor,
+                      final String householdObjectName,
+                      final String householdObjectBrand,
+                      final String mainColour;
                       final boolean isElectric,
                       final int weightKgs,
-                      final int voltage,
+                      final double priceCad, // should this change to purchasePriceCad???? --> revisit
+                      final int voltageV,
+                      final String itemConnectivity;
                       final String powerSource,
-                      final String brandName,
-                      final String modelName,
                       final String cycleTypes,
                       final int decibalRating)
     {
         super(yearPurchased, createdBy, mainColor, isElectric, weightKgs, voltage, powerSource, brandName, modelName);
+        
+        if(decibalRating < MIN_DECIBAL_RATING || decibalRating > MAX_DECIBAL_RATING)
+        {
+            throw new IllegalArgumentException("Invalid decibal rating: " + decibalRating);
+        }
+
+        
         this.cycleTypes    = cycleTypes;
         this.decibalRating = decibalRating;
     }

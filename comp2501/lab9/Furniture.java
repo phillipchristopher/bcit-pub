ebeb2b2f7a835f4533642638f7e028ef0fa69b64
Chris Helmhold <89236634+phillipchristopher@users.noreply.github.com>
2024-03-07 13:01:01 -0800

    Update Furniture.java

--- comp2501/lab9/Furniture.java
+++ comp2501/lab9/Furniture.java
@@ -1,3 +1,19 @@
 public class Furniture extends HouseholdObject
 {
+  private final boolean isAssemblyRequired;
+  private final String frameMaterial;
+  
+  private static final String FRAME_WOOD = "wood";
+  private static final String FRAME_METAL = "metal";
+  private static final String FRAME_OTHER = "other";
+  
+  public Furniture (final String homeObjectName, 
+                    final String homeObjectBrand, 
+                    final double priceCad, 
+                    final String mainColour, 
+                    final boolean isAssemblyRequired, 
+                    final String frameMaterial)
+  {
+    super(homeObjectName, homeObjectBrand, priceCad, mainColour)
+  }
 }

bf00ec0402fdb96ed9cb8b6aca2df7217a99cf8d
Chris Helmhold <89236634+phillipchristopher@users.noreply.github.com>
2024-03-08 11:14:55 -0800

    Update Furniture.java

--- comp2501/lab9/Furniture.java
+++ comp2501/lab9/Furniture.java
@@ -1,3 +1,9 @@
+/**
+* Furniture class
+* @author Chris Helmhold
+* @version 1.0
+*/
+
 public class Furniture extends HouseholdObject
 {
     private final boolean isAssemblyRequired;
@@ -7,6 +13,18 @@ public class Furniture extends HouseholdObject
     private static final String FRAME_METAL = "metal";
     private static final String FRAME_OTHER = "other";
 
+    /**
+    * Constructor for the Furniture class.
+    * @param yearPurchased The year the furniture was purchased.
+    * @param householdObjectName The name of the furniture.
+    * @param householdObjectBrand The brand of the furniture.
+    * @param mainColour The main colour of the furniture.
+    * @param isElectric Whether the furniture is electric.
+    * @param weightKgs The weight of the furniture in kilograms.
+    * @param priceCad The price of the furniture in Canadian dollars.
+    * @param isAssemblyRequired Whether the furniture requires assembly.
+    * @param frameMaterial The frame material of the furniture.
+    */
     public Furniture(final int yearPurchased,
                      final String householdObjectName,
                      final String householdObjectBrand,
@@ -31,11 +49,19 @@ public class Furniture extends HouseholdObject
         this.frameMaterial      = frameMaterial;
     }
 
+    /**
+    * Getter for assembly required.
+    * @return True if assembly is required, false otherwise.
+    */
     public boolean isAssemblyRequired()
     {
         return isAssemblyRequired;
     }
 
+    /**
+    * Getter for frame material.
+    * @return The frame material of the furniture.
+    */
     public String getFrameMaterial()
     {
         return frameMaterial;
